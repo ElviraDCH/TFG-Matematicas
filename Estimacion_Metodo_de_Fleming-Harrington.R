69a79e53155824413d9fef7cbef1318430c2a1c2
ElviraDCH <157234786+ElviraDCH@users.noreply.github.com>
2024-01-21 23:42:01 +0100

    Update Estimacion_Metodo_de_Fleming-Harrington.R

--- Estimacion_Metodo_de_Fleming-Harrington.R
+++ Estimacion_Metodo_de_Fleming-Harrington.R
@@ -2,13 +2,13 @@ library(survival)
 library(KMsurv)
 library(paletteer)
 
-# Datos de los pacientes que recibieron el tratamiento est??ndar
+# Datos de los pacientes que recibieron el tratamiento estándar
 Grupo_1 <-veteran[veteran$trt =='2',] 
 
-#Surv combina informaci??n sobre el tiempo y la censura de los pacientes
+#Surv combina información sobre el tiempo y la censura de los pacientes
 Grupo_1.surv<-Surv(Grupo_1$time, Grupo_1$status)
 
-#survfit calcula las curvas de supervivencia seg??n el estimador indicado, en este caso el de Fleming-Harrington
+#survfit calcula las curvas de supervivencia según el estimador indicado, en este caso el de Fleming-Harrington
 Grupo_1.km <-survfit(Grupo_1.surv ~ 1, data = Grupo_1, type ="fleming-harrington")
 
 #Resumen 
@@ -24,11 +24,11 @@ dc <-data.frame(dx1 =censuras_tiempo, dy1 =censuras_FH )
 IC1 <-data.frame(d1 = Grupo_1.km$time, d2 =Grupo_1.km$upper, d4 =Grupo_1.km$lower)
 
 
-#Representaci??n de la funci??n de supervivencia estimada mediante el m??todo de Fleming-Harrington       
+#Representación de la función de supervivencia estimada mediante el método de Fleming-Harrington       
 ggplot()+
-  geom_step(data = df,aes(x=dx, y=dy, color='2',linetype = '2', shape= '2',fill = '2'),stroke=NA,size=1.1)+
-  geom_point(data = dc,aes(x=dx1, y=dy1, color='1',linetype = '1', shape = '1', fill = '1'),stroke= 1.2,size =1.5)+
   geom_ribbon(data =IC1, aes(x=d1,ymin = d4,ymax =d2,color = '0',linetype = '0', shape = '0', fill = '0'),stroke= NA,size =1.5, alpha = 0.1)+
+  geom_point(data = dc,aes(x=dx1, y=dy1, color='1',linetype = '1', shape = '1', fill = '1'),stroke= 1.2,size =1.5)+
+  geom_step(data = df,aes(x=dx, y=dy, color='2',linetype = '2', shape= '2',fill = '2'),stroke=NA,size=1.1)+
   theme(panel.background = element_rect(fill = 'white'),
         panel.border = element_rect(color = "black", fill=NA, size = 1.5),
         axis.text = element_text(color = "black",size=18),
@@ -40,15 +40,12 @@ ggplot()+
         legend.text = element_text(size=18),
         legend.title = element_blank(),
         legend.spacing.y = unit(0.005, 'cm'))+
-  labs(x='Tiempo en d??as', y='Probabilidad de supervivencia')+
+  labs(x='Tiempo en días', y='Probabilidad de supervivencia')+
   scale_colour_manual(' ',values = c('0'= '#A3123A','1' = 'black','2' = '#BD263E'),
-                      labels = c("Intervalo de confianza","Observaciones censuradas","Estimaci??n Fleming-Harrington"))+
+                      labels = c("Intervalo de confianza","Observaciones censuradas","Estimación Fleming-Harrington"))+
   scale_linetype_manual(' ', values = c('0'=NA, '1'= NA,'2'='solid'),
-                        labels = c("Intervalo de confianza","Observaciones censuradas","Estimaci??n Fleming-Harrington"))+
+                        labels = c("Intervalo de confianza","Observaciones censuradas","Estimación Fleming-Harrington"))+
   scale_shape_manual(' ', values = c('0'=NA,'1'= 3,'2'= NA),
-                     labels = c("Intervalo de confianza","Observaciones censuradas","Estimaci??n Fleming-Harrington"))+
+                     labels = c("Intervalo de confianza","Observaciones censuradas","Estimación Fleming-Harrington"))+
   scale_fill_manual(' ', values = c('0'='#A3123A', '1'= 'white', '2'= 'white'),
-                    labels = c("Intervalo de confianza","Observaciones censuradas","Estimaci??n Fleming-Harrington"))
-
-
-
+                    labels = c("Intervalo de confianza","Observaciones censuradas","Estimación Fleming-Harrington"))
