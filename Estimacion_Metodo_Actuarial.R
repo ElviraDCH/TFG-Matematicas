6a3246ac722e3c77315715a18f01f3012c6bcc0f
ElviraDCH <157234786+ElviraDCH@users.noreply.github.com>
2024-01-21 19:53:24 +0100

    Update Estimacion_Metodo_Actuarial.R

--- Estimacion_Metodo_Actuarial.R
+++ Estimacion_Metodo_Actuarial.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(paletteer)
 library(ggplot2)
 
-#Estimacion de la funcion de supervivencia mediante el metodo actuarial
+#Estimación de la función de supervivencia mediante el método actuarial
 S <- c(0.679, 0.515, 0.312, 0.211, 0.158, 0.088, 0.070, 0.035, 0.018, 0.018,
        0.018, 0,0)
 
-#Division de la duracion del estudio en intervalos de 50 dias 
+#División de la duración del estudio en intervalos de 50 dias 
 t <- seq(0,600,50)
 
 #Valores censurados
@@ -17,7 +17,7 @@ cy <-c(0.679,0.515,0.312,0.312,0.211)
 df <- data.frame(dx=t, dy=S)
 dp <-data.frame(dx1=cx,dy1 =cy)
 
-#Representacion
+#Representación
 ggplot()+
   geom_step(data = df,aes(x=dx, y=dy, color='0',linetype = '0', shape= '0'),stroke=NA,size=1.2)+
   geom_point(data = dp,aes(x=dx1, y=dy1, color='1',linetype = '1', shape = '1'),stroke= 1.2,size =1.7)+
@@ -32,10 +32,10 @@ ggplot()+
         legend.text = element_text(size=18),
         legend.title = element_blank(),
         legend.spacing.y = unit(0.01, 'cm'))+
-  labs(x='Tiempo en dias', y='Probabilidad de supervivencia')+
+  labs(x='Tiempo en días', y='Probabilidad de supervivencia')+
   scale_colour_manual(' ',values = c('0' = '#BD263E','1' = 'black'),
-                      labels = c( "Estimacion actuarial", "Observaciones censuradas"))+
+                      labels = c( "Estimación actuarial", "Observaciones censuradas"))+
   scale_linetype_manual(' ', values = c('0'='solid', '1'= NA),
-                        labels = c( "Estimacion actuarial", "Observaciones censuradas"))+
+                        labels = c( "Estimación actuarial", "Observaciones censuradas"))+
   scale_shape_manual(' ', values = c('0'= NA, '1'= 3),
-                     labels = c( "Estimacion actuarial", "Observaciones censuradas"))
+                     labels = c( "Estimación actuarial", "Observaciones censuradas"))
