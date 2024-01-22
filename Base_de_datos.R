c72637f8784defd7adc8bcdda005a75ed6892077
ElviraDCH <157234786+ElviraDCH@users.noreply.github.com>
2024-01-22 12:18:48 +0100

    Update Base_de_datos.R

--- Base_de_datos.R
+++ Base_de_datos.R
@@ -1,4 +1,4 @@
 library(survival)
 
-# Base de datos utilizada en los ejemplos de los cap??tulos 4 y 5 del trabajo 
+# Base de datos utilizada en los ejemplos de los capítulos 4 y 5 del trabajo 
 Datos <- veteran
