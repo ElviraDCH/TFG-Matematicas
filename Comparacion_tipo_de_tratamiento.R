5a48080c73426a9a0fd5153e16b5200fa72e09ea
ElviraDCH <157234786+ElviraDCH@users.noreply.github.com>
2024-01-22 11:33:35 +0100

    Update Comparacion_tipo_de_tratamiento.R

--- Comparacion_tipo_de_tratamiento.R
+++ Comparacion_tipo_de_tratamiento.R
@@ -3,34 +3,34 @@ library(survival)
 library(KMsurv)
 library(paletteer)
 
-#Pacientes que recibieron el tratamiento estándar
+# Pacientes que recibieron el tratamiento estándar
 Grupo_1 <-veteran[veteran$trt!='2',] 
 
-#Pacientes que recibieron el tratamiento de quimioterapia 
+# Pacientes que recibieron el tratamiento de quimioterapia 
 Grupo_2 <-veteran[veteran$trt!='1',] 
 
 
-#Objetos Surv y Survfit para cada grupo de tratamiento mediante el estimador de Kaplan-Meier
+# Objetos Surv y Survfit para cada grupo de tratamiento mediante el estimador de Kaplan-Meier
 Grupo1.surv <-Surv(Grupo_1$time,Grupo_1$status)
 Grupo1.km <-survfit(Grupo1.surv ~ 1, data = Grupo_1, type ="kaplan-meier")
 
 Grupo2.surv <- Surv(Grupo_2$time, Grupo_2$status) 
 Grupo2.km <- survfit(Grupo2.surv ~ 1, data = Grupo_2, type = "kaplan-meier")
 
-#Resumen datos de supervivencia para cada grupo 
+# Resumen datos de supervivencia para cada grupo 
 summary(Grupo1.km)
 summary(Grupo2.km)
 
-#Datos censurados del grupo 1-tratamiento estándar
+# Datos censurados del grupo 1-tratamiento estándar
 censuras_KM <-c(0.7536,0.5172,0.5020,0.3922,0.2124)
 censuras_tiempo <-c(25, 97,100, 123,182)
 
-#Datos censurados del grupo 2-tratamiento de quimioterapia
+# Datos censurados del grupo 2-tratamiento de quimioterapia
 censuras_KM2 <-c(0.4265,0.3960,0.3326,0.1830)
 censuras_tiempo2 <-c(83,87,103,231)
 
 
-#DataFrames
+# DataFrames
 df <-data.frame(dx =Grupo1.km$time, dy = Grupo1.km$surv)
 dc <-data.frame(dx1 =censuras_tiempo, dy1 =censuras_KM )
 IC1 <-data.frame(d1 = Grupo1.km$time, d2 =Grupo1.km$upper, d4 =Grupo1.km$lower)
@@ -39,7 +39,7 @@ df2 <-data.frame(dx2 =Grupo2.km$time, dy2 = Grupo2.km$surv)
 dc2 <-data.frame(dx12 =censuras_tiempo2, dy12 =censuras_KM2 )
 IC2 <-data.frame(d12 = Grupo2.km$time, d22 =Grupo2.km$upper, d42 =Grupo2.km$lower)
 
-#Representación de la estimación de supervivencia para cada grupo de tratamiento 
+# Representación de la estimación de supervivencia para cada grupo de tratamiento 
 ggplot()+
   geom_step(data = df,aes(x=dx, y=dy, color='0',linetype = '0', shape= '0', fill = '0'),stroke=NA,size=1.1)+
   geom_step(data = df2,aes(x=dx2, y=dy2, color='1',linetype = '1', shape= '1', fill = '1'),stroke=NA,size=1.1)+
