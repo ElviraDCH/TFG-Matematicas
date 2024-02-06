76b5f89a417c1db07064990759213489c0eaedfb
ElviraDCH <157234786+ElviraDCH@users.noreply.github.com>
2024-02-06 22:31:43 +0100

    Update Ejemplo_4_2.R

--- Ejemplo_4_2.R
+++ Ejemplo_4_2.R
@@ -4,15 +4,15 @@ library(ggplot2)
 library(survminer)
 library(ggpubr)
 
-#Datos de los pacientes con c??lulas cancer??genas tipo Large y Adeno
+#Datos de los pacientes con células cancerígenas tipo Large y Adeno
 Large <-veteran[veteran$celltype =='large',]
 Adeno <-veteran[veteran$celltype =='adeno',]
 
-#Objeto Surv y funcion survfit para cada tipo de celula 
+#Objeto Surv y funcion survfit para cada tipo de célula 
 Large.surv <- Surv(Large$time, Large$status)
 Adeno.surv <- Surv(Adeno$time, Adeno$status)
 
-#Se calcula la funci??n de riesgo acumulado
+#Se calcula la función de riesgo acumulado
 mod1<-coxph(Large.surv~1, data=Large, method="breslow")
 mod2<-coxph(Adeno.surv~1, data=Adeno, method="breslow")
 bh1 <- basehaz(mod1)
@@ -26,22 +26,22 @@ lntime2<-log(bh2[,2])
 dfLarge <- data.frame(dx = lntime1, dy = lnhazard1)
 dfAdeno <- data.frame(dx1 = lntime2, dy1 = lnhazard2)
 
-#DataFrame UNICAMENTE para las celulas Large y Adeno
+#DataFrame ÚNICAMENTE para las celulas Large y Adeno
 juntos <-merge(Large, Adeno, all = TRUE)
 
-#Estimaci??n de los coeficientes del modelo
+#Estimación de los coeficientes del modelo
 fitcomp <- coxph(Surv(time, status)~celltype, data=juntos)
 
-#Verificaci??n de las hip??tesis de riesgo proporcionales
+#Verificación de las hipótesis de riesgo proporcionales
 ftest <- cox.zph(fitcomp)
 
 #p-valor para cada una de las covariables
 p_valor <- ftest
 
-#Gr??fica de los residuos de Schoenfeld
+#Gráfica de los residuos de Schoenfeld
 ggcoxzph(ftest)
 
-#Representaci??n de las curvas de supervivencia log-log 
+#Representación de las curvas de supervivencia log-log 
 ggplot()+
   geom_line(data = dfLarge,aes(x=dx, y=dy, color='0'),size=1.1)+
   geom_line(data = dfAdeno,aes(x=dx1, y=dy1, color='1'),size=1.1)+
